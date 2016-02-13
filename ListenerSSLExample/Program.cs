14e8e4006fcb831f80fb2b24178750eaf10c2496
James Nocentini <S401309@emirates.com>
2016-02-13 16:32:36 +0400

    change cert method

--- ListenerSSLExample/Program.cs
+++ ListenerSSLExample/Program.cs
@@ -17,18 +17,20 @@ namespace ListenerSSLExample
         [STAThread]
         static void Main()
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+           
 
             Manager manager = Manager.SharedInstance;
             var database = manager.GetDatabase("kis-database");
 
-            var cert = X509Manager.GetExistingPersistentCertificate("127.0.0.1", "123abc", System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "unit_test.pfx"));
+            var cert = X509Manager.GetPersistentCertificate("127.0.0.1", "123abc", System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "unit_test.pfx"));
             Uri uri = new Uri(String.Format("http://localhost:{0}/{1}/", 3689, "kis-database"));
             CouchbaseLiteTcpListener listener = new CouchbaseLiteTcpListener(manager, 3689, CouchbaseLiteTcpOptions.UseTLS, cert);
 
             listener.Start();
+
+             Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
